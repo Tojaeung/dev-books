366dd5481824d9c91f5dd5e1d259aa1b8933ba85
Tojaeung <kimwoon11@naver.com>
2022-12-05 19:00:10 +0900

    이펙티브자바 item2

--- src/이펙티브자바/item1/ch1_이름소유/Student.java
+++ src/이펙티브자바/item1/ch1_이름소유/Student.java
@@ -1,28 +1,28 @@
 package 이펙티브자바.item1.ch1_이름소유;
 
 public class Student {
-	private String name;
-	private int age;
-	private int classroom;
+    private String name;
+    private int age;
+    private int classroom;
 
-	// 이름을 가진 정적 팩토리 메서드
-	public static Student fromName(String name) {
-		Student student = new Student();
-		student.name = name;
-		return student;
-	}
+    // 이름을 가진 정적 팩토리 메서드
+    public static Student fromName(String name) {
+        Student student = new Student();
+        student.name = name;
+        return student;
+    }
 
-	// 생성자 시그니처 제약없음
-	public static Student fromAge(int age) {
-		Student student = new Student();
-		student.age = age;
-		return student;
-	}
+    // 생성자 시그니처 제약없음
+    public static Student fromAge(int age) {
+        Student student = new Student();
+        student.age = age;
+        return student;
+    }
 
-	// 생성자 시그니처 제약없음
-	public static Student fromClassroom(int classroom) {
-		Student student = new Student();
-		student.classroom = classroom;
-		return student;
-	}
+    // 생성자 시그니처 제약없음
+    public static Student fromClassroom(int classroom) {
+        Student student = new Student();
+        student.classroom = classroom;
+        return student;
+    }
 }
